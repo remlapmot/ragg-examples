976b762a1a50fa41c38bfa6c0a52d9dd99f5c221
Tom Palmer <remlapmot@hotmail.com>
2025-08-26 15:33:54 +0100

    Create example-1.R

---
+++ example-1.R
@@ -0,0 +1,18 @@
+# Examples from grDevices::plotmath helpfil
+if (Sys.info()['sysname'] == "Darwin") os <- "macos"
+if (Sys.info()['sysname'] == "Linux") os <- "linux"
+
+library(ragg)
+library(glue)
+
+agg_png(glue('img/example-1-ragg-plot-{os}.png'))
+par(mar = c(5, 5, 5, 5), cex.lab = 1.3)
+plot(1:10, xlab = expression(hat(beta)), ylab = expression(hat(beta)))
+title("Plot exported with agg_png()")
+dev.off()
+
+png(glue('img/example-1-png-plot-{os}.png'))
+par(mar = c(5, 5, 5, 5), cex.lab = 1.3)
+plot(1:10, xlab = expression(hat(beta)), ylab = expression(hat(beta)))
+title("Plot exported with png()")
+dev.off()
