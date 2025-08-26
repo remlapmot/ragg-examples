c177b62c9e5ea486974697fe4a036bf1b89d5ce2
Tom Palmer <remlapmot@hotmail.com>
2025-08-26 16:44:24 +0100

    Increase resolution

--- example-1.R
+++ example-1.R
@@ -6,13 +6,16 @@ if (Sys.info()['sysname'] == "Windows") os <- "windows"
 library(ragg)
 library(glue)
 
-agg_png(glue('img/example-1-ragg-plot-{os}.png'))
+reso <- 300
+scl <- reso/72
+
+agg_png(glue('img/example-1-ragg-plot-{os}.png'), units = "px", res = reso, height = scl*480, width = scl*480)
 par(mar = c(5, 5, 5, 5), cex.lab = 1.3)
 plot(1:10, xlab = expression(hat(beta)), ylab = expression(hat(beta)))
 title("Plot exported with agg_png()")
 dev.off()
 
-png(glue('img/example-1-png-plot-{os}.png'))
+png(glue('img/example-1-png-plot-{os}.png'), units = "px", res = reso, height = scl*480, width = scl*480)
 par(mar = c(5, 5, 5, 5), cex.lab = 1.3)
 plot(1:10, xlab = expression(hat(beta)), ylab = expression(hat(beta)))
 title("Plot exported with png()")
